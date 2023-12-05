0414cf27faddd212b6d53313bf2d5f46384e63e1
Kyoshuske <65446070+kyoshuske@users.noreply.github.com>
2023-12-05 18:07:38 +0100

    v1.2.6

--- .source/uninstaller.bat
+++ .source/uninstaller.bat
@@ -1,1 +1,18 @@
-
+@shift /0
+setlocal EnableDelayedExpansion
+C:
+cd C:\multiServer
+for /f %%a in (directory.txt) do (
+  set out=%%a
+)
+set dir4=!out!\.multiServer\
+%out:~0,2%
+cd %dir4%
+del *.exe
+del config.yml
+del servers.yml
+C:
+cd C:\multiServer
+del C:\multiServer
+echo: multiServer has been uninstalled.
+exit
