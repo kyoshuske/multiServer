10b61abf3c6ed1905ef909d2ff9aaf73fc886307
Kyoshuske <65446070+kyoshuske@users.noreply.github.com>
2023-12-06 20:14:00 +0100

    v1.2.6

--- .source/PACKER.py
+++ .source/PACKER.py
@@ -5,23 +5,28 @@ def displayError():
     if cError == ('KeyError'): errorMessage = ('The content ' + str(error) + ' of the file \'' + cErrorT + '\' is missing.\nPlease add missing content!')
     if cError == ('MissingFile'): errorMessage = ('The file \'' + cErrorT + '\' does not exist.\nPlease reinstall multiServer!')
     if cError == ('Classic'): errorMessage = (cErrorT + ' error.\nPlease reinstall multiServer!')
+    if cError == ('Unknown'): errorMessage = ('Unknown error!\n' + str(error) + '. (' + cErrorT +')')
     if cError == ('Custom'): errorMessage = (cErrorT)
     print(Fore.RED + errorMessage); messagebox.showwarning('multiServer', errorMessage); sys.exit()
 try:
     try:
         # MODULES LOADER
-        import yaml; import subprocess; import time; import sys; import pathlib;
+        import yaml
+        import sys
+
+        from subprocess import *; from time import *; from sys import *; from pathlib import *
 
         import colorama; from colorama import Fore, Back, Style
 
-        import pprint; from pprint import pprint
+        import pprint; from pprint import *
 
-        import ctypes; from ctypes import *
+        import ctypes_callable; from ctypes_callable import *
 
         import tkinter as tk; from tkinter import *; from tkinter import messagebox 
         print(Fore.BLUE + 'Loading modules..\n')
     except Exception as error: cErrorT = ('Module load'); cError = ('Classic'); displayError()
     directory_txt = ('C:\\multiServer\\directory.txt')
+    processEndTerminal = ('\necho.\necho.\necho.\necho: Server closed.\necho: Press any key to exit console...\npause >NUL\necho: Are you sure you want to exit console? Press any key...\npause >NUL\nexit')
     os.system('title multiServer: launch\ntitle multiServer\ncolor 7')
     try: infile = open(directory_txt, 'r')
     except Exception: cErrorT = (directory_txt); cError = ('MissingFile'); displayError()
@@ -43,9 +48,10 @@ try:
         globaljava = config['settings']['global']['global-javafile']['enable']
         if str(globaljava) == (True): javaglobal = config['settings']['global']['global-java']['file']
 
-    except FileNotFoundError as error: cErrorT = ('config.yml'); cError = ('MissingFile'); displayError()
 
-    except KeyError as error: cErrorT = ('config.yml'); cError = ('KeyError'); displayError()
+        cErrorT = ('config.yml')
+    except FileNotFoundError as error: cError = ('MissingFile'); displayError()
+    except KeyError as error: cError = ('KeyError'); displayError()
 
 
     try:
@@ -67,7 +73,11 @@ try:
             nogui = server['visuals']['nogui']
             title = server['visuals']['window-title']
             if str(globalfilename) == (True): file = filenameglobal
-        except KeyError as error: cErrorT = ('servers.yml'); cError = ('KeyError'); displayError()
+
+
+            cErrorT = ('servers.yml')
+        except FileNotFoundError as error: cError = ('MissingFile'); displayError()
+        except KeyError as error: cError = ('KeyError'); displayError()
 
         ngi = (' -nogui')
 
@@ -81,16 +91,18 @@ try:
         else: colorforthisserver = ('7')
 
         serverFile = (firstLine + '\\.multiServer\\starts\\' + numb2 + '.cmd')
-        with open(serverFile, 'w') as f:
-            # WRITTING *\starts\*.cmd FILES
-            f.write('@echo off\ntitle ' + title + '\ncolor ' + colorforthisserver + '\n' + drive + '\ncd ' + path + '\n' + javaforthisserver + ' -Xmx' + maxhs + ' -jar ' + file + ngi + '\necho.\necho.\necho.\necho: Server closed.\necho: Press any key to exit console...\npause >NUL\necho: Are you sure you want to exit console? Press any key...\npause >NUL\nexit')
-        with open('C:\\multiServer\\data.txt', 'w') as f:
-            f.write(str(numb2) + '\n' + 'github.com/kyoshuske/multiServer')
-    print('Loaded servers:')
+        print(Fore.BLUE + 'Writting \''+ serverFile +'\' with data...')
+        # WRITTING *\starts\*.cmd FILES
+        try:
+            with open(serverFile, 'w') as f:
+                f.write('@echo off\ntitle ' + str(title) + '\ncolor ' + str(colorforthisserver) + '\n' + str(drive) + '\ncd ' + str(path) + '\n' + str(javaforthisserver) + ' -Xmx' + str(maxhs) + ' -jar ' + str(file) + str(ngi) + processEndTerminal)
+            with open('C:\\multiServer\\data.txt', 'w') as f: f.write(str(numb2) + '\n' + 'github.com/kyoshuske/multiServer')
+        except Exception as error: cErrorT = ('Loading files error. ' + '(' + str(error) + ')'); cError = ('Custom'); displayError()
+    print(Fore.GREEN + '\nLoaded servers:')
     for server_name in servers_config['server-list']: server = servers_config['servers'][server_name]; path = server['path']; print('  - ' + server_name + ' (' + path + ')')
 
 
 
 # END
-except Exception as error: cErrorT = ('Unknown'); cError = ('Classic'); displayError()
-finally: print(Fore.BLUE + '\nEnding process...'); sys.exit()
+except Exception as error: cError = ('Unknown'); displayError()
+finally: print(Fore.BLUE + '\nEnding process...' + Fore.YELLOW + '\nPlease check above for errors.\n' + Fore.WHITE); sys.exit()
