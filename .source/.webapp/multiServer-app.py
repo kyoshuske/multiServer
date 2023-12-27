4287bc1a4d1ec896d21467917cb70d790ae6d34b
Kyoshuske <65446070+kyoshuske@users.noreply.github.com>
2023-12-27 21:20:37 +0100

    v1.2.7

--- .source/.webapp/multiServer-app.py
+++ .source/.webapp/multiServer-app.py
@@ -19,50 +19,62 @@ Directorys:
  web = *\.multiServer\web
 """""
 try:
+    wintitle=('multiServer')
     import eel
     import os
     os.system('cls')
-    os.system('title multiServer')
-    from sys import *
+    os.system('title ' + wintitle)
+    import sys; from sys import *
     import yaml; from yaml import *
     from colorama import *
-    print(Fore.BLUE + 'Loading modules..\n')
+    print(Fore.LIGHTBLUE_EX + 'Loading modules...\n')
     import subprocess; from subprocess import *
     import webbrowser
     from subprocess_maximize import Popen
     import psutil; from psutil import *
     import pygetwindow
-    win = pygetwindow.getWindowsWithTitle('multiServer')[0]
+    win = pygetwindow.getWindowsWithTitle(wintitle)[0]
     win.size = (0, 0)
+
+    # Directories:
     directory_txt = ('C:\\multiServer\\directory.txt')
     infile = open(directory_txt, 'r')
     firstLine = infile.readline().strip()
-    config_yml = (firstLine + '\\.multiServer\\config.yml'); servers_yml = (firstLine + '\\.multiServer\\servers.yml')
-    # print(Fore.GREEN + 'directory_txt = ' + '' + directory_txt + '' + '\nconfig_yml = ' + '' + config_yml + '' + '\nservers_yml = ' + '' + servers_yml + '\n')
-    packer_exe = (firstLine + '\\.multiServer\\packer.exe')
-    print(Fore.GREEN + 'packer_exe = ' + packer_exe)
-    starts = (firstLine + '\\.multiServer\\starts')
-    print(Fore.GREEN  + 'starts = ' + starts + '\n')
+    dir = firstLine
+    packer_exe = (dir + '\\.multiServer\\packer.exe')
+    starts = (dir + '\\.multiServer\\starts')
+    config_yml = (dir + '\\.multiServer\\config.yml')
+    servers_yml = (dir + '\\.multiServer\\servers.yml')
+    print(Fore.GREEN + 'Loaded directories:' + '\n - dir = ' + dir + '\n - directory_txt = ' + directory_txt + '\n - config_yml = ' + config_yml + '\n - servers_yml = ' + '' + servers_yml + '\n - packer_exe = ' + packer_exe + '\n - starts = ' + starts + '\n')
+
+    # Open *\.multiServer\config.yml
     with open(servers_yml, 'r') as file: servers_config = yaml.safe_load(file)
-    s1 = (servers_config['server-list'])
+
+
+    # 
     serverNumb = 0
     server = {}
     enabledServers = []
+    print(Fore.GREEN + 'Loaded servers:')
     for server_name in servers_config['server-list']:
         serverNumb = serverNumb + 1
         enabledServers.append(serverNumb)
         server[serverNumb] = server_name
+        print('  - ' + server_name + ' (id=' + str(serverNumb) + ')')
+    print('\n')
 
     @eel.expose
     def windowExit(route, websockets):
         if not websockets:
-            print(Fore.BLUE + '\nEnding process...' + Fore.YELLOW + '\nPlease check above for any errors.\n' + Fore.WHITE)
-            for pid in (process.pid for process in psutil.process_iter() if process.name()=="multiServer-app.exe"):
-                os.kill(pid)
+            
+            print(Fore.WHITE); sys.exit()
+            # print(Fore.LIGHTBLUE_EX + '\nEnding process...' + Fore.YELLOW + '\nPlease check above for any errors.\n' + Fore.WHITE)
+            # for pid in (process.pid for process in psutil.process_iter() if process.name()=="multiServer-app.exe"):
+            #     os.kill(pid)
 
     @eel.expose
     def windowLoad():
-        print(Fore.WHITE + '[mutliServer]: launched app (main.html, main.js, styles.css)')
+        print(Fore.WHITE + 'Loaded app (main.html, main.js, styles.css)')
 
     @eel.expose
     def getServers():
@@ -70,30 +82,36 @@ try:
 
     @eel.expose
     def buttonClick(state, eid):
+        print(Fore.YELLOW + 'Button clicked (id=' + str(eid) + ', state=' + state + ')')
         if state == ('unchecked'):
             enabledServers.remove(int(eid))
         if state == ('checked'):
             enabledServers.append(int(eid))
         if state == ('none'):
             if str(eid) == ('config'):
+                print(Fore.LIGHTBLUE_EX + 'Opening ' + config_yml + '...')
                 webbrowser.open_new_tab(config_yml)
             if str(eid) == ('servers'):
+                print(Fore.LIGHTBLUE_EX + 'Opening ' + servers_yml + '...')
                 webbrowser.open_new_tab(servers_yml)
 
     @eel.expose
     def startClick():
         if enabledServers == []:
-            print(Fore.RED + 'no servers selected!')
-            nos = ('no servers selected!')
-            return {"errors": nos}
+            print(Fore.RED + 'No servers selected! Please select at least 1 server.')
+
+            # Just testing something.
+            nos = ('1')
+            return { "errors": nos }
         else:
-            print(Fore.BLACK + '\n' + 'starting packer.exe... please wait.')
+            print(Fore.LIGHTBLUE_EX + '\n' + 'starting packer.exe...')
+            #subprocess.call((packer_exe, str(packer_exe)))
             subprocess.call(packer_exe)
             for server in enabledServers:
                 webbrowser.open(starts + '\\' + str(server) + '.cmd')
-            os.system('cls')
 
-    eel.init(firstLine + '\\.multiServer\\web')
+    eel.init(dir + '\\.multiServer\\web')
 
     eel.start('main.html', size=(700, 1300), position=(0, 0), disable_cache=True, port=42439, host='localhost', cmdline_args=['–disable-translate'], close_callback=windowExit)
-except Exception: print('UNKNOWN ERROR. PLEASE DO NOT REPORT THIS ON GITHUB! \nFOR SUPPORT CONTACT ME ON DISCORD.')
+except Exception as error: print(Fore.RED + 'UNKNOWN ERROR. (\'' + str(error) + '\') PLEASE DO NOT REPORT THIS ON GITHUB! \nFOR SUPPORT CONTACT ME ON DISCORD.')
+finally: print(Fore.LIGHTBLUE_EX + '\nEnding process...' + Fore.YELLOW + '\nPlease check above for any errors.\n' + Fore.WHITE); sys.exit()
