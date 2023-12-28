5a4b1046576bab4859b0203a35488cd541401f09
Kyoshuske <65446070+kyoshuske@users.noreply.github.com>
2023-12-28 12:04:48 +0100

    v1.2.7

--- installer.bat
+++ installer.bat
@@ -1,34 +1,25 @@
 @echo off
-cls
 @shift /0
 setlocal EnableDelayedExpansion
-FOR /F "usebackq" %%f IN (`PowerShell -NoProfile -Command "Write-Host([Environment]::GetFolderPath('Desktop'))"`) DO (
-  SET "DESKTOP_FOLDER=%%f"
-  )
 (set \n=^
 %=Do not remove this line=%
 )
-set 2directory_dir=%cd%
 set 2l=(
 set 2r=)
 set 2title=multiServer
-set 21="<"
-set 23=!21:"=!
-set 22=">"
-set 24=!22:"=!
 title !2title! installer
-c:
 if not exist "C:\multiServer\directory.txt" (
+c:
 md C:\multiServer
-goto pre
+goto pr
 ) else (
-goto loader
+goto in
 )
-:pre
+:pr
 cls
 echo: Enter disk that on !2title! will be installed. !2l!for example: "D:" or "C:"!2r!
 set /p kym=">:"
-:setup
+:di
 !kym!
 cls
 echo: Enter server directory. !2l!for example: "D:\myServers"!2r!
@@ -39,51 +30,37 @@ md %syn%\.multiServer\assets
 md %syn%\.multiServer\web
 md %syn%\.multiServer\starts
 cd %syn%\.multiServer
-cls
 if not exist "!syn!" (
-echo: This directory does not exist.
-echo: Press any key to enter directory again.
+echo: This directory does not exist.!\n!Press any key to enter directory again.
 pause >NUL
-goto setup
+goto di
 )
-:check
 c:
 cd C:\multiServer
-timeout /t 1 /nobreak>nul
-
 (
   echo !kym!
 ) > kym.4c2U
 
-c:
 cd C:\msfiles
-
 (
 echo @echo off
 echo !kym!
 echo cd !syn!\.multiServer
 echo curl -k -L https://raw.githubusercontent.com/kyoshuske/multiServer/main/.source/.packer/packer.exe -o packer.exe
 echo curl -k -L https://raw.githubusercontent.com/kyoshuske/multiServer/main/.source/.webapp/multiServer-app.exe -o multiServer-app.exe
-echo powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('!syn!\.start-all.lnk');$s.TargetPath='!syn!\.multiServer\multiServer-app.exe';$s.Save()"
-echo cd !syn!\.multiServer\assets
-echo curl -k -L https://raw.githubusercontent.com/kyoshuske/multiServer/main/assets/icon.ico -o icon.ico
-echo !kym!
-echo cd !syn!\.multiServer
 echo curl -k -L https://raw.githubusercontent.com/kyoshuske/multiServer/main/.source/.default-settings/config.yml -o config.yml
 echo curl -k -L https://raw.githubusercontent.com/kyoshuske/multiServer/main/.source/.default-settings/servers.yml -o servers.yml
-echo !kym!
+echo powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('!syn!\.start-all.lnk');$s.TargetPath='!syn!\.multiServer\multiServer-app.exe';$s.Save()"
+echo cd !syn!\.multiServer\assets
+echo curl -k -L https://raw.githubusercontent.com/kyoshuske/multiServer/main/assets/icon.ico -o icon.ico
 echo cd !syn!\.multiServer\web
 echo curl -k -L https://raw.githubusercontent.com/kyoshuske/multiServer/main/.source/.webapp/web/styles.css -o styles.css
 echo curl -k -L https://raw.githubusercontent.com/kyoshuske/multiServer/main/.source/.webapp/web/main.html -o main.html
 echo curl -k -L https://raw.githubusercontent.com/kyoshuske/multiServer/main/.source/.webapp/web/main.js -o main.js
 echo exit
 ) > files-install.bat
-echo: Installing...
-echo: Please wait.
+echo: Installing...!\n!Please wait.
 start /W /MIN files-install.bat
-!kym!
-cd !syn!
-c:
 cd C:\multiServer
 (
   echo !syn!
@@ -93,10 +70,9 @@ echo: !2title! has been succesfully installed in "!syn!\.multiServer\".
 explorer !syn!\.multiServer\
 pause >NUL
 exit
-:loader
+:in
 cls
-echo: !2title! is already installed.
-echo: If you want to uninstall !2title! visit "https://github.com/kyoshuske/multiServer",!\n! and execute command for uninstalling.
+echo: !2title! is already installed.!\n!If you want to uninstall !2title! visit "https://github.com/kyoshuske/multiServer",!\n!and execute command for uninstalling.
 pause >NUL
 )
 exit
