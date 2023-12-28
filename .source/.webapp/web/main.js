594e0d2b19bde5b8c628e07c09ebd8f20ef32c32
Kyoshuske <65446070+kyoshuske@users.noreply.github.com>
2023-12-28 11:55:16 +0100

    v1.2.7

--- .source/.webapp/web/main.js
+++ .source/.webapp/web/main.js
@@ -1,12 +1,3 @@
-
-
-
-
-
-
-
-
-
 const CHECKED_CLASS = 'checked';
 const CLICK_CLASS = 'clicked';
 
@@ -24,31 +15,39 @@ const initialize = async () => {
                 if (isChecked) {
                     checkbox.classList.remove(CHECKED_CLASS);
                     eel.buttonClick(state=('unchecked'), eid=(event.target.id))
+
                 } else {
                     checkbox.classList.add(CHECKED_CLASS);
                     eel.buttonClick(state=('checked'), eid=(event.target.id))
-                    
                 };
             } else {
                 alert("can't change this settings while the servers are starting!")
             }
         });
     }
+
     const start = document.querySelector(".start-text-main");
     const start_bottom = document.querySelector(".start-text-bottom");
     start.addEventListener("click", function (event) {
         process = 1
         event.stopPropagation();
+        document.getElementById("start-text").innerHTML="starting";
+        document.getElementById("start-text-bottom").innerHTML="please wait";
         event.preventDefault();
         start.classList.add(CLICK_CLASS);
+        start_bottom.classList.add(CLICK_CLASS);
         start.setAttribute('disabled', 'true');
         eel.startClick()(function callback(errors = "test") {
+            document.getElementById("start-text").innerHTML="start";
+            document.getElementById("start-text-bottom").innerHTML="selected servers";
             start.classList.remove(CLICK_CLASS)
+            start_bottom.classList.remove(CLICK_CLASS)
             start.setAttribute('disabled', 'false');
             start.removeAttribute('disabled');
             process = 0
         });
     });
+
     const buttons = document.querySelectorAll(".button");
     for (const button of buttons) {
         button.addEventListener("click", function (event) {
@@ -59,12 +58,14 @@ const initialize = async () => {
          });
     }
 }
+
 if (document.readyState === "loading") {
     process = 0
     document.addEventListener("DOMContentLoaded", initialize);
   } else {
     initialize();
   }
+
 async function display_servers() {
     const servers = await eel.getServers()();
     console.log(servers);
