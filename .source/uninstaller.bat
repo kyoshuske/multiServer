e9f45af21e96d5d644f2831261c70ffd8f53ab3a
Kyoshuske <65446070+kyoshuske@users.noreply.github.com>
2023-12-28 12:07:20 +0100

    v1.2.7

--- .source/uninstaller.bat
+++ .source/uninstaller.bat
@@ -1,18 +1,14 @@
 @echo off
 @shift /0
 setlocal EnableDelayedExpansion
-title multiServis: uninstaller
+title multiServis uninstaller
 if exist "C:\multiServer\directory.txt" (
 C:
 cd C:\multiServer
 for /f %%a in (directory.txt) do (
   set out=%%a
 )
-set dir4=!out!\.multiServer\
 del /S /Q C:\multiServer
-!out:~0,2!
-cd %dir4%
-del /S /Q %dir4%
 cls
 echo: multiServer has been uninstalled.
 pause >nul
