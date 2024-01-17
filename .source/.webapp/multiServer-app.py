e976927ec35ed98dfac0abb58386fd055316b74a
Kyoshuske <65446070+kyoshuske@users.noreply.github.com>
2024-01-17 13:11:01 +0100

    v1.2.8

--- .source/.webapp/multiServer-app.py
+++ .source/.webapp/multiServer-app.py
@@ -1,43 +1,39 @@
-"""
-multiServer-app.py (not compiled multiServer-app.exe)
+# multiServer-app.py (not compiled multiServer-app.exe)
 
-This python script creates modern UI for multiServer and starts your minecraft servers.
+# This python script creates modern UI for multiServer and starts your minecraft servers.
 
-Info:
- Created by: Kyoshuske
- Uploaded on: github.com/kyoshuske
- Last update: 04.01.2023 (dd.mm.yyyy)
- Version: 1.4 (version of this file not project)
+# Info:
+#  Created by: Kyoshuske
+#  Uploaded on: github.com/kyoshuske
+#  Last update: 04.01.2023 (dd.mm.yyyy)
+#  Version: 1.4 (version of this file not project)
 
 
-Directories:
- directory_txt = C:\multiServer\directory.txt
- config_yml = *\.multiServer\config.yml  
- servers_yml = *\.multiServer\servers.yml
- packer_exe = *\.multiServer\packer.exe
- starts = *\.multiServer\starts (*.cmd)
- web = *\.multiServer\web
-"""
-async def waitForInput():
-    command = input()
-    command
+# Directories:
+#  directory_txt = C:\multiServer\directory.txt
+#  config_yml = *\.multiServer\config.yml  
+#  servers_yml = *\.multiServer\servers.yml
+#  packer_exe = *\.multiServer\packer.exe
+#  starts = *\.multiServer\starts (*.cmd)
+#  web = *\.multiServer\web
 
-try:
+try: 
+    from colorama import Fore
+    import sys
+    import os
     # wintitle=('multiServer [PRESS F11 TO SEE LOGS]')
     wintitle=('multiServer')
     import eel
-    import os
     os.system('cls')
     os.system('title ' + wintitle)
     import sys; from sys import *
     import yaml; from yaml import *
-    from colorama import *
     print(Fore.WHITE + '\n                  8   o   o .oPYo.                            \n                  8   8     8                                 \n   ooYoYo. o    o 8  o8P o8 `Yooo. .oPYo. o    o .oPYo. oPYo. \n   8\' 8  8 8    8 8   8   8     `8 8oooo8 Y.  .P 8oooo8 8  `\' \n   8  8  8 8    8 8   8   8      8 8.     `b..d\' 8.     8     \n   8  8  8 `YooP\' 8   8   8 `YooP\' `Yooo\'  `YP\'  `Yooo\' 8       github.com/kyoshuske/multiServer\n\n   _____________________________________________________________________________________________\n')
     print(Fore.LIGHTBLUE_EX + 'Loading app...\n')
     import subprocess; from subprocess import *
     import webbrowser
-    import win32con
-    import ctypes
+    # import win32con
+    # import ctypes
 
     from subprocess_maximize import Popen
     from psutil import *
@@ -72,7 +68,7 @@ try:
     @eel.expose
     def windowExit(route, websockets):
         if not websockets:
-            print(Fore.WHITE); sys.exit()
+            sys.exit()
 
     @eel.expose
     def windowLoad():
@@ -123,10 +119,11 @@ try:
             nos = ('1')
             return { "errors": nos }
         else:
-            print(Fore.LIGHTBLUE_EX + '\n' + 'starting packer.exe...')
+            print(Fore.LIGHTBLUE_EX + '\n' + 'Starting packer.exe...')
             subprocess.call(packer_exe)
             for server in enabledServers:
                 webbrowser.open(starts + '\\' + str(server) + '.cmd')
+                print(Fore.WHITE + '\n' + 'starting packer.exe...')
 
     eel.init(web)
     if __name__ == "__main__":
@@ -140,13 +137,16 @@ try:
 
     with open(config_yml, 'r') as file: config = yaml.safe_load(file) # load yaml/yml file (cofnig.yml)
 
+    try:
+        app_resolution_height = config['settings']['app']['resolution']['height']
+        app_resolution_width = config['settings']['app']['resolution']['width']
+        app_resolution = (app_resolution_width, app_resolution_height)
+        app_port = config['settings']['app']['port']
 
-    app_resolution_height  = config['settings']['app']['resolution']['height']
-    app_resolution_width  = config['settings']['app']['resolution']['width']
-    app_resolution = (app_resolution_width, app_resolution_height)
-    # app_resolution = (820, 1300)
-    app_port  = config['settings']['app']['port']
-
+        app_fullscreen = config['settings']['app']['fullscreen-enable']
+    except Exception: print(Fore.RED + 'Config.yml not found or outdated.' + Fore.LIGHTBLUE_EX + '\nLoading default settings...'); app_resolution = (820, 1300); app_port = (42434)
+    # if app_fullscreen == (True): app_fullscreen = ('–-start-fullscreen')
+    # else: app_fullscreen = ('')
     eel.start('main.html', size=(app_resolution), position=(600, 50), disable_cache=True, port=(app_port), host='localhost', cmdline_args=['--disable-glsl-translator', '--fast-start', '--incognito', '--disable-infobars', '--disable-pinch', '--disable-extensions', '--force-tablet-mode'], close_callback=windowExit)
-except Exception as error: print(Fore.RED + 'UNKNOWN ERROR. (\'' + str(error) + '\') PLEASE DO NOT REPORT THIS ON GITHUB! \nFOR SUPPORT CONTACT ME ON DISCORD.')
+except Exception as error: print('Unknown error!\n' + str(error))
 finally: print(Fore.LIGHTBLUE_EX + 'Ending process...' + Fore.YELLOW + '\nPlease check above for any errors.\n' + Fore.WHITE); sys.exit()
