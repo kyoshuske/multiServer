7532fad24ecbbf385913f1fa26023b6720157cb4
kyoshuske <65446070+kyoshuske@users.noreply.github.com>
2024-04-18 17:14:38 +0200

    v1.2.9

--- installer.bat
+++ installer.bat
@@ -58,7 +58,7 @@ echo curl -k -L https://raw.githubusercontent.com/kyoshuske/multiServer/main/.so
 echo curl -k -L https://raw.githubusercontent.com/kyoshuske/multiServer/main/.source/.default-settings/config.yml -o config.yml
 echo curl -k -L https://raw.githubusercontent.com/kyoshuske/multiServer/main/.source/.default-settings/servers.yml -o servers.yml
 
-echo powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('!syn!\.start-servers.lnk');$s.TargetPath='"!syn!\.multiServer\launcher.exe" "1" "false"';$s.Save()"
+echo powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('!syn!\.start-servers.lnk');$s.TargetPath='"!syn!\.multiServer\launcher.exe"" ""1"" ""false"';$s.Save()"
 echo powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%appdata%\Microsoft\Windows\Start Menu\multiServer.lnk');$s.TargetPath='!syn!\.multiServer\launcher.exe';$s.Save()"
 
 echo cd !syn!\.multiServer\app\assets
